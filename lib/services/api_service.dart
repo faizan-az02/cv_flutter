5e2b217f738835da31eaeefaeb25c55a0946e876
Faizan Aziz <faizan.professional02@gmail.com>
2025-08-09 20:27:55 +0500

    Returning the data from API as well as the status code

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,17 +2,29 @@ import 'dart:convert';
 //import 'dart:io';
 import 'package:http/http.dart' as http;
 
+class ApiResponse {
+  final int statusCode;
+  final Map<String, dynamic>? body;
+
+  ApiResponse({required this.statusCode, this.body});
+}
+
 class ApiService {
   // Get the appropriate base URL based on platform
-  static String baseUrl = 'http://192.168.100.9:5000';
+  static String baseUrl = 'http://192.168.100.6:5000';
 
-  static Future<Map<String, dynamic>> fetchData() async {
+  static Future<ApiResponse> fetchData() async {
+  try {
     final response = await http.get(Uri.parse('$baseUrl/data'));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return data;
-    } else {
-      throw Exception('Failed to load data');
-    }
+    return ApiResponse(
+      statusCode: response.statusCode,
+      body: response.statusCode == 200 ? json.decode(response.body) : null,
+    );
+  } catch (e) {
+    return ApiResponse(
+      statusCode: 0, // 0 means no HTTP response received
+      body: {'error': e.toString()},
+    );
   }
+}
 }
