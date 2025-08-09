48cee247ec65ac760576cc29ba3f57da224a415d
Faizan Aziz <faizan.professional02@gmail.com>
2025-08-09 20:58:18 +0500

    Added timeout

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -14,17 +14,20 @@ class ApiService {
   static String baseUrl = 'http://192.168.100.6:5000';
 
   static Future<ApiResponse> fetchData() async {
-  try {
-    final response = await http.get(Uri.parse('$baseUrl/data'));
-    return ApiResponse(
-      statusCode: response.statusCode,
-      body: response.statusCode == 200 ? json.decode(response.body) : null,
+    try {
+          final response = await http.get(Uri.parse('$baseUrl/data')).timeout(
+      const Duration(seconds: 5),
     );
-  } catch (e) {
-    return ApiResponse(
-      statusCode: 0, // 0 means no HTTP response received
-      body: {'error': e.toString()},
-    );
+      
+      return ApiResponse(
+        statusCode: response.statusCode,
+        body: response.statusCode == 200 ? json.decode(response.body) : null,
+      );
+    } catch (e) {
+      return ApiResponse(
+        statusCode: 0, // 0 means no HTTP response received (timeout/connection refused)
+        body: {'error': e.toString()},
+      );
+    }
   }
-}
 }
